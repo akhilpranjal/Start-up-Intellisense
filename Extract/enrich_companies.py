00f92d3a9296407111b53bc68cb51a3dc137c800
Pranjal Srivastava <akhilpranjal11@gmail.com>
2026-06-08 23:16:25 +0530

    renaming model name

--- Extract/enrich_companies.py
+++ Extract/enrich_companies.py
@@ -13,7 +13,7 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-GROQ_MODEL = os.getenv("GROQ_MODEL")
+GROQ_MODEL = os.getenv("STRUCTURING_GROQ_MODEL")
 
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL not found")
